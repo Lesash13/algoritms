21bf3dc87b403c1cd7ed29866c38c78a5b803f9e
Lesash13 <18631883+lesash13@users.noreply.github.com>
2023-02-03 14:20:38 +0300

    fix zero,hands

--- introduction_to_algorithms_final_tasks/zero.py
+++ introduction_to_algorithms_final_tasks/zero.py
@@ -1,17 +1,17 @@
-# ID = 81771281
+# ID = 81771506
 from typing import List
 
 
 def count_index(length, street, zero_indexes) -> List[int]:
-    res: List[int] = []
+    res: List[int] = [0] * length
     previous_value_res: int = 0
     for i in range(0, length):
         if street[i] == 0:
-            res.append(0)
+            res[i] = 0
             continue
         nearest: int = abs(i - zero_indexes[0])
         if len(zero_indexes) == 1:
-            res.append(nearest)
+            res[i] = nearest
             continue
         for j in range(previous_value_res, len(zero_indexes)):
             min_value: int = abs(i - zero_indexes[j])
@@ -21,7 +21,7 @@ def count_index(length, street, zero_indexes) -> List[int]:
             if min_value > nearest:
                 break
 
-        res.append(nearest)
+        res[i] = nearest
     return res
 
 
